366f7dddfcc4651b572da86fe2a48976583b4e6c
jirqoadai <jirqoadai@qq.com>
2019-05-29 16:20:53 +0800

    add https

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
+var fs = require('fs');
+var https = require('https');
+
+var options = {
+  key: fs.readFileSync('./keys/server.key'),
+  cert:fs.readFileSync('./keys/server.crt')
+}
 
 app = express();
 // 自定义跨域中间件
@@ -57,7 +64,6 @@ app.get('/chinaz', function (req, res, next) {
 
 });
 
-
 app.get('/discount', function (req, res, next) {
   var items = [];
   // 用 superagent 去抓取 https://cnodejs.org/ 的内容
@@ -93,10 +99,54 @@ app.get('/discount', function (req, res, next) {
       console.log(items);
       res.send(items);
     });
+});
 
+
+app.get('/api/discount', function (req, res, next) {
+  var items = [];
+  // 用 superagent 去抓取 https://cnodejs.org/ 的内容
+  superagent.get('http://appstore-discounts.com/list.php')
+    .query({
+      sort:'appFree'
+    })
+    .end(function (err, sres) {
+      // 常规的错误处理
+      if (err) {
+        return next(err);
+      }
+      // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+      // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+      // 剩下就都是 jquery 的内容了
+      var $ = cheerio.load(sres.text);
+
+      $('.product-grid > div').each(function (idx, element) {
+        var $element = $(element);
+        if ($element.hasClass('adsGrid')) {
+          console.log('get the ad');
+        } else {
+          items.push({
+            name: $element.find('.name').text(),
+            price: $element.find('.price').text(),
+            newFromeDate: $element.find('.newFromDate').text(),
+            category: $element.find('.category').text(),
+            icon: $element.find('.image').find('img').attr('src'),
+            url: $element.find('.cart').find('input').eq(1).attr('onclick')
+          });
+        }
+      });
+      console.log(items);
+      res.send(items);
+    });
 });
 
 
 app.listen(3000, function (req, res) {
   console.log('app is running at port 3000');
 });
+
+https.createServer(options, app).listen(20087, (err) => {
+  if (err) {
+      throw err
+  }
+  console.log(`Application https worker ${process.pid} started ... at port 20087`);
+})
